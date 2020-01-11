66f45fe8497f1da6c53a45eb266ae00b5ca3f80f
onno204 <15333546+onno204@users.noreply.github.com>
2020-01-11 16:25:44 +0100

    Added example data

---
+++ userdata_example.js
@@ -0,0 +1,4 @@
+module.exports = [
+    {channelId: "665334562879963138", token: "21342134"},
+    // {channelId: "665196463881584644", email: "farmer1@example.nl", password: "VeryCoolPassword"},
+]
