bcb665ca3eadca285b9b7f4739c53077ab21e779
onno204 <15333546+onno204@users.noreply.github.com>
2020-01-13 03:32:15 +0100

    Added buggy voice

---
+++ StreamDispatcher.js
@@ -0,0 +1,161 @@
+'use strict';
+
+// Source: https://github.com/discordjs/discord.js/blob/master/src/client/voice/dispatcher/StreamDispatcher.js
+
+const { Writable, Transform } = require('stream');
+const dgram = require('dgram');
+const randomBytes = require('randombytes');
+const sodium = require('sodium-javascript');
+const ytdl = require('ytdl-core');
+const youtubeStream = require('youtube-audio-stream');
+const prism = require('prism-media');
+const { workerData } = require('worker_threads');
+const Speaker = require('speaker');
+const speaker = new Speaker({
+    channels: 2,          // 2 channels
+    bitDepth: 16,         // 16-bit samples
+    sampleRate: 48000     // 44,100 Hz sample rate
+});
+
+const obj = workerData.obj;
+const discord_voice_opts = workerData.discord_voice_opts
+const FFMPEG_ARGUMENTS = [
+  '-analyzeduration', '0',
+  '-loglevel', '0',
+  '-f', 's16le',
+  '-ar', '48000',
+  '-ac', '2',
+//   '-c:a', 'libopus',
+//   '-b:a', '16K'
+];
+const client = dgram.createSocket('udp4');
+
+function voice_play_youtubelink(url){
+    return voice_play_from_url(url)
+    ytdl.getInfo(url, { filter: 'audioonly', quality: 'highestaudio' }, (err, data) => {
+        if (err) throw err;
+        var format = ytdl.chooseFormat(data.formats, { quality: 'highestaudio' });
+        if (format) {
+            console.log('Format found: ', format);
+            voice_play_from_url(format['url'])
+        }
+    })
+}
+function voice_play_from_url(url) { 
+    var stream = ytdl(url, { filter: 'audioonly' }, { passes: 3 })
+    // const stream = youtubeStream(url)
+    console.log("running from uri: ", url)
+    var args = FFMPEG_ARGUMENTS.slice() //['-i', url, ...FFMPEG_ARGUMENTS]
+    const ffmpeg = new prism.FFmpeg({ args });
+    const streams = { ffmpeg };
+    stream.pipe(ffmpeg);
+    return voice_play_youtubelink_playstream(ffmpeg, streams);
+}
+function voice_play_youtubelink_playstream(stream, streams){
+    // var encoder = new opusscript(48000, 2, opusscript.Application.AUDIO);
+    // const opus = streams.opus = encoder.encode(stream, 960);
+    const opus = streams.opus = new prism.opus.Encoder({ channels: 2, rate: 48000, frameSize: 960 });
+    // const debug1 = new debugStream("opus");
+    // stream.pipe(debug1);
+    // debug1.pipe(streams.opus)
+    // stream.pipe(speaker);
+    const dispatcher = new StreamDispatcher(obj, streams);
+    streams.opus.pipe(dispatcher)
+}
+
+
+
+class debugStream extends Transform {
+    constructor(name){
+        super(name);
+        this.name = name;
+        this.count = 0;
+    }
+    _transform(data, _, done) {
+        console.log('['+this.name+"] tranform: ", this.count++);
+        this.push(data);
+        done()
+    }
+    _flush(done){ done(); }
+    _final(done){ done(); }
+
+}
+
+
+class StreamDispatcher extends Writable {
+    constructor(self, streams){
+        super(self, streams);
+        this.self = self;
+        this.streams = streams;
+
+        this.on('finish', () => {
+        });
+        const streamError = (type, err) => {
+          if (type && err) {
+            err.message = `${type} stream: ${err.message}`;
+          }
+        };
+        this.count = 0
+
+        this.on('error', () => streamError());
+        if (this.streams.ffmpeg) this.streams.ffmpeg.on('error', err => streamError('ffmpeg', err));
+        if (this.streams.opus) this.streams.opus.on('error', err => streamError('opus', err));
+    }
+    _write(chunk, enc, done) {
+        if (!this.startTime) {
+            this.startTime = Date.now();
+            this._sdata = {
+                sequence: 0,
+                timestamp: 0,
+            }
+        }
+        this._step(done)
+        this.voice_send_chunk(chunk);
+        // console.log("writting: ", this.count)
+    }
+    _step(done) {
+      this._sdata.sequence++;
+      this._sdata.timestamp += (48000 / 100) * 2;
+      if (this._sdata.sequence >= 2 ** 16) this._sdata.sequence = 0;
+      if (this._sdata.timestamp >= 2 ** 32) this._sdata.timestamp = 0;
+      this.count++;
+      done()
+    }
+    voice_encrypt(buffer) {
+        // xsalsa20_poly1305_suffix
+        var random = randomBytes(24)
+        var cipher = Buffer.alloc(buffer.length + sodium.crypto_secretbox_MACBYTES)
+        sodium.crypto_secretbox_easy(cipher, buffer, random, this.self.voice_security_data.secret_key)
+        return [cipher, random];
+    }
+    voice_createPacket(sequence, timestamp, buffer) {
+        const packetBuffer = Buffer.alloc(12);
+        packetBuffer[0] = 0x80;
+        packetBuffer[1] = 0x78;
+
+        packetBuffer.writeUIntBE(sequence, 2, 2);
+        packetBuffer.writeUIntBE(timestamp, 4, 4);
+        packetBuffer.writeUIntBE(this.self.voice.connection_data.ssrc, 8, 4);
+
+        packetBuffer.copy(Buffer.alloc(24), 0, 0, 12);
+        // packetBuffer.copy(this.self.voice_nonceBuffer, 0, 0, 12);
+        var encrypted = this.voice_encrypt(buffer);
+        return Buffer.concat([packetBuffer, ...encrypted]);
+    }
+    voice_send_chunk(chunk){
+        const client = dgram.createSocket('udp4');
+        var packet = this.voice_createPacket(this._sdata.sequence, this._sdata.timestamp, chunk)
+        this.voice_send_packet(packet);
+    }
+    voice_send_packet(packet){
+        var self = this.self
+        var serverdata = self.voice.connection_data;
+        client.send(packet, 0, packet.length, serverdata.port, serverdata.ip, (e) => {
+            if (e) {
+                console.error("[VOICE] Failed to send a packet: ", e);
+            }
+        });
+    }
+}
+
+voice_play_youtubelink(workerData.url)
--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 const axios = require('axios')
 const WebSocket = require('ws');
+const dgram = require('dgram');
 const jsdom = require("jsdom");
+const { Worker, isMainThread, parentPort, workerData } = require('worker_threads');
+
+
 // https://github.com/websockets/ws
 
 
@@ -8,14 +12,28 @@ const discord_opts = {
     DISPATCH: 0,
     HEARTBEAT: 1,
     IDENTIFY: 2,
+    VOICE: 4,
     INVALID_SESSION: 9,
     HELLO: 10,
     HEARTBEAT_RECEIVED: 11
+}
+const discord_voice_opts = {
+    IDENTIFY: 0,
+    SELECT_PROTOCOL: 1,
+    READY: 2,
+    HEARTBEAT: 3,
+    SESSION_DESCRIPTION: 4,
+    SPEAKING: 5 ,
+    HEARTBEAT_RECEIVED: 6,
+    HELLO: 8,
+    CONNECTED: 12,
+    DISCONNECTED: 13,
 }
 const DiscordAPI = {
     BASE: "https://discordapp.com/api/",
     LOGIN: "auth/login",
     MESSAGE: "channels/%s1/messages",
+    GUILD_ID: "665191411188236288"
 }
 
 String.prototype.format = function(){
@@ -62,6 +80,9 @@ function login_with_email(email, password, channelId){
 }
 
 class DiscordBot{
+    /////////////////////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////                    setup                  /////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////////////////////////
     constructor(token, targetChannel){
         this.info = {
             targetChannelId: targetChannel,
@@ -74,18 +95,17 @@ class DiscordBot{
             proceed_running: false,
         }
         this.active = false
+        this.voice = {
+            active: false,
+            data: undefined,
+            connection_data: undefined,
+        }
         this.login_gateway();
     }
-
-    message_get_quoted_string(msg){
-        var tmpStr  = msg.match("`(.*)`");
-        return tmpStr[1];
-    }
-    
     login_gateway(){
-        var self = this;
         this.auth.heartbeat_interval = undefined;
         const ws = new WebSocket("wss://gateway.discord.gg/?v=6&encoding=json", { perMessageDeflate: false });
+        var self = this;
         ws.on('open', function open() {
             // ws.send('something');
         });
@@ -117,66 +137,84 @@ class DiscordBot{
                             }
                         }
                     }
-                    console.log("sending: ", identification)
                     ws.send(JSON.stringify(identification))
                 }
             }else if (data.op == discord_opts.HEARTBEAT){
                 self.login_gateway_send_heartbeat(ws);
             }else if (data.op == discord_opts.FAILED_OR_ZOMBIE){
-                killSlave("FAILED OR ZOMBIE", ws)
+                self.killSlave("FAILED OR ZOMBIE", ws)
             }else if (data.op == discord_opts.DISPATCH){
                 self.auth.heartbeat_s = data.s
-                if(data.t == "READY"){
-                    self.info.userinfo = data.d;
-                    console.log("READY: ", self.info.userinfo.user.username)
-                    if (self.auth.proceed_running === true){ return false; }
-                    self.auth.proceed_running = true;
-                    self.login_gateway_send_heartbeat(ws);
-                    setInterval(function(){
+                switch (data.t) {
+                    case "READY":
+                        self.info.userinfo = data.d;
+                        console.log("READY: ", self.info.userinfo.user.username)
+                        if (self.auth.proceed_running === true){ return false; }
+                        self.auth.proceed_running = true;
                         self.login_gateway_send_heartbeat(ws);
-                    }, self.auth.heartbeat_interval);
-                    self.active = true;
-                    self.handler();
-                }else if(data.t == "PRESENCE_UPDATE"){
-                    return true;
-                }else if(data.t == "MESSAGE_UPDATE"){
-                    return true;
-                }else if (data.t == "GUILD_CREATE"){
-                    self.active = false
-                }else if(data.t == "MESSAGE_CREATE"){
-                    if(data.d.channel_id === self.info.targetChannelId){
-                        self.message_received(data.d); 
-                    }
-                }else if(data.t == "PRESENCE_UPDATE"){
-                    return true;
-                }else{
-                    console.error("Unkown event: ", data.t);
+                        setInterval(function(){
+                            self.login_gateway_send_heartbeat(ws);
+                        }, self.auth.heartbeat_interval);
+                        self.active = true;
+                        self.handler(ws);
+                        break;
+
+                    case "PRESENCE_UPDATE":
+                        break;
+
+                    case "MESSAGE_UPDATE":
+                        break;
+
+                    case "GUILD_CREATE":
+                        self.active = false
+                        self.killSlave("Guild not found", ws)
+                        break;
+
+                    case "MESSAGE_CREATE":
+                        if(data.d.channel_id === self.info.targetChannelId){
+                            self.message_received(data.d); 
+                        }
+                        break;
+
+                    case "PRESENCE_UPDATE":
+                        break;
+
+                    case "SESSIONS_REPLACE":
+                        break;
+
+                    case "VOICE_SERVER_UPDATE":
+                        if(self.voice.data !== undefined){
+                            self.voice_gateway(ws, data.d.token, data.d.guild_id, data.d.endpoint, self.voice.data.user_id, self.voice.data.session_id)
+                            self.voice.data = undefined
+                        }else{
+                            self.voice.data = data.d
+                        }
+                        break;
+
+                    case "VOICE_STATE_UPDATE":
+                        if(self.voice.data !== undefined){
+                            self.voice_gateway(ws, self.voice.data.token, self.voice.data.guild_id, self.voice.data.endpoint, data.d.user_id, data.d.session_id)
+                            self.voice.data = undefined
+                        }else{
+                            self.voice.data = data.d
+                        }
+                        break;
+                
+                    default:
+                        console.log("Unkown event: ", data.t)
+                        break;
                 }
             }else if (data.op == discord_opts.INVALID_SESSION){
-                killSlave("Invalid Session", ws)
+                self.killSlave("Invalid Session", ws)
             }else if (data.op == discord_opts.HEARTBEAT_RECEIVED){
                 // console.log("HEARTBEAT_RECEIVED");
             }else{
                 console.error("Discord gateway unkown/handled: ", data);
-                killSlave("Unkown opt! It is safer to kill", ws)
+                self.killSlave("Unkown opt! It is safer to kill", ws)
                 return false;
             }
         });
     }
-    killSlave(reason, ws){
-        console.error("["+self.info.userinfo.user.email+"] Slave has been killed. " + reason)
-        console.error("["+self.info.userinfo.user.email+"] Slave has been killed. " + reason)
-        console.error("["+self.info.userinfo.user.email+"] Slave has been killed. " + reason)
-        console.error("["+self.info.userinfo.user.email+"] Slave has been killed. " + reason)
-        console.error("["+self.info.userinfo.user.email+"] Slave has been killed. " + reason)
-        console.error("["+self.info.userinfo.user.email+"] Slave has been killed. " + reason)
-        console.error("["+self.info.userinfo.user.email+"] Slave has been killed. " + reason)
-        console.error("["+self.info.userinfo.user.email+"] Slave has been killed. " + reason)
-        console.error("["+self.info.userinfo.user.email+"] Slave has been killed. " + reason)
-        ws.close();
-        process.exit();
-    }
-
     login_gateway_send_heartbeat(ws){
         var heartbeat = {
             "op": discord_opts.HEARTBEAT,
@@ -185,8 +223,24 @@ class DiscordBot{
         // console.log("sending heartbeat: ", JSON.stringify(heartbeat))
         ws.send(JSON.stringify(heartbeat))
     }
-
-    
+    /////////////////////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////                    MISC                   /////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    killSlave(reason, ws){
+        console.log(reason);
+        console.error("["+this.info.userinfo.user.email+"] Slave has been killed. " + reason)
+        console.error("["+this.info.userinfo.user.email+"] Slave has been killed. " + reason)
+        console.error("["+this.info.userinfo.user.email+"] Slave has been killed. " + reason)
+        console.error("["+this.info.userinfo.user.email+"] Slave has been killed. " + reason)
+        console.error("["+this.info.userinfo.user.email+"] Slave has been killed. " + reason)
+        console.error("["+this.info.userinfo.user.email+"] Slave has been killed. " + reason)
+        console.error("["+this.info.userinfo.user.email+"] Slave has been killed. " + reason)
+        console.error("["+this.info.userinfo.user.email+"] Slave has been killed. " + reason)
+        console.error("["+this.info.userinfo.user.email+"] Slave has been killed. " + reason)
+        ws.close();
+        this.active = false
+        process.exit();
+    }
     getDomFromHTMLString(html){
         const {JSDOM} = jsdom;
         const dom = new JSDOM(html);
@@ -230,14 +284,10 @@ class DiscordBot{
             callback(error.response.data)
         })
     }
-    sendMessage(message){
-        if(this.active !== true){ return false; }
-        this.postRequest(DiscordAPI.BASE + DiscordAPI.MESSAGE.format(this.info.targetChannelId), {content: message}, function(data){
-            // console.log("send message: ", message)
-        })
-    }
 
-
+/////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////               TEXT CHANNELS               /////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////
     message_received(data){
         if (data.embeds.length >= 1){
             var embeds = data.embeds[0];
@@ -310,8 +360,8 @@ class DiscordBot{
             this.sendMessage( this.message_get_quoted_string(msg) );
         }
     }
-
-    handler(){
+    handler(ws){
+        this.joinVoiceChannel(ws, "665670000916430859")
         this.sendMessage("pls daily")
         var self = this
         setTimeout(function(){
@@ -355,5 +405,205 @@ class DiscordBot{
             }, 1000 * 10);
         }, 1000 * 10);
     }
+
+
+    sendMessage(message){
+        if(this.active !== true){ return false; }
+        this.postRequest(DiscordAPI.BASE + DiscordAPI.MESSAGE.format(this.info.targetChannelId), {content: message}, function(data){
+            // console.log("send message: ", message)
+        })
+    }
+    message_get_quoted_string(msg){
+        var tmpStr  = msg.match("`(.*)`");
+        return tmpStr[1];
+    }
+
+
+
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////              VOICE CHANNELS               /////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////
+    joinVoiceChannel(ws, id){
+        var voiceData = {
+            "op": discord_opts.VOICE,
+            "d": {
+                "guild_id": DiscordAPI.GUILD_ID,
+                "channel_id": id,
+                "self_mute": false,
+                "self_deaf": false
+            }
+        }
+        console.log("JSON.stringify(voice.data): ", JSON.stringify(voiceData))
+        ws.send(JSON.stringify(voiceData))
+    }
+    voice_gateway(ws, token, guild_id, endpoint, user_id, session_id){
+        if (endpoint !== undefined){
+            console.log("data: ", token, guild_id, endpoint, user_id, session_id)
+            this.voice.active = false
+            this.voice.heartbeat_interval = undefined;
+            var self = this;
+            const wsv = this.wsv = new WebSocket("wss://"+endpoint.split(':')[0]+"/?v=4&encoding=json", { perMessageDeflate: false });
+            wsv.on('open', function open() {
+                // wsv.send('something');
+            });
+            wsv.on('error', function incoming(data) {
+                console.log("Voice server Error: ", data)
+                self.killSlave("Voice server error", ws)
+            });
+            wsv.on('ready', function incoming(data) {
+                // console.log("READY: ", data)
+            });
+            wsv.on('message', function incoming(data) {
+                data = JSON.parse(data);
+                if (data.op == discord_voice_opts.HELLO){
+                    if(data.d.heartbeat_interval != undefined){
+                        self.voice.heartbeat_interval = data.d.heartbeat_interval;
+                        var identification = {
+                            "op": discord_voice_opts.IDENTIFY,
+                            "d": {
+                                "server_id": DiscordAPI.GUILD_ID,
+                                "user_id": user_id,
+                                "session_id": session_id,
+                                "token": token,
+                            }
+                        }
+                        console.log("[voice]sending identification: ", identification)
+                        wsv.send(JSON.stringify(identification))
+                    }
+                }else if (data.op == discord_voice_opts.HEARTBEAT){
+                    self.voice_gateway_send_heartbeat();
+                }else if (data.op == discord_voice_opts.READY){
+                    self.voice.active = true
+                    console.log("VOICE DATA: ", data)
+                    self.voice.connection_data = data.d
+                    self.voice_gateway_send_heartbeat();
+                    setInterval(function(){
+                        self.voice_gateway_send_heartbeat();
+                    }, self.voice.heartbeat_interval);
+                    self.voice_connect()
+                }else if (data.op == discord_voice_opts.DISCONNECTED){
+                    // User disconnected
+                    // self.voice.active = false
+                    // wsv.close()
+                }else if (data.op == discord_voice_opts.CONNECTED){
+
+                }else if (data.op == discord_voice_opts.SESSION_DESCRIPTION){
+                    self.voice_security_data = data.d
+                    self.voice_send_test()
+                    console.log("self.voice_security_data: ", self.voice_security_data)
+                }else if (data.op == discord_voice_opts.HEARTBEAT_RECEIVED){
+                    // console.log("HEARTBEAT_RECEIVED");
+                }else if (data.op == discord_voice_opts.SPEAKING){
+                    console.log("speaking")
+                    return true
+                }else{
+                    console.error("[VOICE] Discord gateway unkown/handled: ", data);
+                    self.killSlave("[VOICE] Unkown opt! It is safer to kill", ws)
+                    return false;
+                }
+            });
+        }
+    }
+    voice_gateway_send_heartbeat(){
+        this.voice_nonce = (new Date()).getTime()- 1578860524514
+        if (this.voice_nonce > ((2 ** 32) - 1)) { this.voice_nonce = 0; }
+        if (this.voice_nonceBuffer === undefined){ this.voice_nonceBuffer = Buffer.alloc(24); }
+        this.voice_nonceBuffer.writeUInt32BE(this.voice_nonce, 0);
+        var heartbeat = {
+            "op": discord_voice_opts.HEARTBEAT,
+            "d": this.voice_nonce
+        }
+        console.log("sending heartbeat: ", JSON.stringify(heartbeat))
+        this.wsv.send(JSON.stringify(heartbeat))
+    }
+    voice_connect(){
+        this.voice_start_udp()
+        var self = this;
+        axios.get('https://api.ipify.org/?format=json').then(function (response) {
+            var externalIp = response.ip
+            var serverdata = self.voice.connection_data
+            var voiceData = {
+                "op": discord_voice_opts.SELECT_PROTOCOL,
+                "d": {
+                    "protocol": "udp",
+                    "data": {
+                        "address": externalIp,
+                        "port": 4024,
+                        "mode": "xsalsa20_poly1305_suffix"
+                    }
+                }
+            }
+            self.wsv.send(JSON.stringify(voiceData))
+        })
+    }
+    voice_set_speaking(status){
+        var bitStatus = 0;
+        if (status){ bitStatus = 1 << 0; }
+        var voiceData = {
+            "op": discord_voice_opts.SPEAKING,
+            "d": {
+                "speaking": bitStatus,
+                "delay": 0,
+                "ssrc": this.voice.connection_data.ssrc
+            }
+        }
+        this.wsv.send(JSON.stringify(voiceData))
+    }
+    voice_send_test(){
+        this.voice_set_speaking(true)
+        const worker = new Worker("./StreamDispatcher.js", {
+            workerData: {
+                obj: {
+                    voice_security_data: this.voice_security_data,
+                    voice: this.voice
+                },
+                discord_voice_opts: discord_voice_opts,
+                url: 'https://www.youtube.com/watch?v=lKfAoHnLstk'
+            }
+        });
+        worker.on('message', (e) => console.log("Message: ", e));
+        worker.on('error', (e) => console.log("Error: ", e));
+        worker.on('exit', (e) => {
+            this.voice_set_speaking(false)
+            if (e !== 0)
+                console.log("Error-code: ", e)
+        });
+    }
+    voice_start_udp(){
+        var self = this;
+        const server = this.voiceServer = dgram.createSocket('udp4');
+        server.on('error', (err) => {
+            console.log(`server error:\n${err.stack}`);
+            server.close();
+        });
+        server.on('message', (msg, rinfo) => {
+            // var decoded = this.opusEncoder.decode( msg  , this.opusFrame_size );
+            // console.log(`[VOICE] server got: ${msg} from ${rinfo.address}:${rinfo.port}`);
+            // console.log(`[VOICE] server got message from ${rinfo.address}`);
+        });
+        server.on('listening', function() {
+            const address = server.address();
+            console.log(`[VOICE] server listening ${address.address}:${address.port}`);
+        });
+
+        server.bind(4024);
+        const discoveryMessage = Buffer.alloc(70);
+        discoveryMessage.writeUIntBE(this.voice.connection_data.ssrc, 0, 4);
+        this.voice_send_packet(discoveryMessage);
+    }
+    voice_send_packet(packet){
+        var self = this
+        var serverdata = this.voice.connection_data;
+        const client = dgram.createSocket('udp4');
+        client.send(packet, 0, packet.length, serverdata.port, serverdata.ip, (e) => {
+            if (e) {
+                console.error("[VOICE] Failed to send a packet: ", e);
+            }
+        });
+    }
+
 }
 login()
